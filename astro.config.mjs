86e70df68e696b6bd01f998ef0f5d27248a5ede5
Frank Stallone <frank@designzen.co>
2024-02-15 05:30:23 -0500

    config: ⚙<fe0f> Adding a few essentials like partytown and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({applyBaseStyles: false,})]
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), partytown(), sitemap()]
 });
